2fe38731d43ffd025ba9bf1b4da5c89f4d148184
Ryan LaBouve <imryanlabouve@gmail.com>
2017-10-12 22:35:20 -0500

    Works

--- main.js
+++ main.js
@@ -1,60 +1,50 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const { app, Tray, Menu, BrowserWindow } = require('electron');
+const path = require('path');
+const moment = require('moment');
 
-const path = require('path')
-const url = require('url')
+let appIcon = null;
+let win = null;
+let isOpen = null;
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let time = () => {
+  return moment().format('h:mm a');
+};
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+let timeHour = () => {
+  return moment().format('h');
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+let contextMenu = () => {
+  return Menu.buildFromTemplate([
+    {
+      label: time()
+    },
+    {
+      type: 'separator'
+    },
+    {
+      label: 'quit',
+      accelerator: 'Command+q',
+      selector: 'terminate:'
+    }
+  ]);
+};
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
+let iconPath = () =>  path.join(__dirname, 'icons', `${timeHour()}-Template.png`);
+
+let setupClock = (tray) => {
+  tray.setToolTip(time());
+  tray.setImage(iconPath());
+  if (!isOpen) {
+    tray.setContextMenu(contextMenu());
   }
-})
+}
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
+app.on('ready', () => {
+  win = new BrowserWindow({ show: false });
+  tray = new Tray(iconPath());
+  setInterval(() => setupClock(tray), 1000);
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+  tray.on('mouse-enter', () => { isOpen = true; });
+  tray.on('mouse-leave', () => { isOpen = false; });
+});
